afd4731bbd1b59b3668fc2e4ae9fe914b9403b95
losrobbos <losrobbos@web.de>
2022-08-26 14:55:17 +0200

    Revert "Autsch! Das gibt jetzt richtig Ärger"
    This reverts commit 70fe8b2950823351d656038f925205c8c437798a.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 console.log("Erster Commit, alles noch toll!")
 
 console.log("Authsch! Das wird gleich schiefgehen! Aber egal. Ich pushe ja nicht!")
-
-console.lo("Jetzt wirds richtig wild! Ich werd gepusht und alle werden mich hassen!")
